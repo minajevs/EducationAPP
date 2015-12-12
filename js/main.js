aabf258a3ad65ec7a2db3e47146fa10184692704
minajevs <d3x133@gmail.com>
2015-12-12 13:47:35 +0200

    Initial

--- js/main.js
+++ js/main.js
@@ -1,1 +1,186 @@
+$( document ).ready(function() {
+    //task1
+    $('#task1title').text(task1.title);
+    $('#task1author').text(task1.author);
+    task1.text.forEach(function(el){
+        $('#task1text').append('<p>' + el + '</p>');
+    });
 
+    $('#task1author').text(task1.author);
+    $('#task1footer').hide();
+    $('#task1results').hide();
+
+    for(var i = 1; i < 6; i++){
+        $('#t1q' + i + 't').text(task1.questions[i-1].text)
+        for(var j = 1; j < 4; j++){
+            $('#t1q' + i + 'a' + j).text(task1.questions[i-1].options[j-1]);
+        }
+    }
+    //task2
+    $('#task2p2').hide();
+    $('#task2footer').hide();
+    $('#task2results').hide();
+    $('#task2p1').text(task2.text1);
+    for(var i = 1; i < 7; i++) {
+        $('#task2q' + i + 't').text(task2.text2[i-1]);
+    };
+    //task3
+    $('#task3word').text(task3.words[0]);
+    $('#task3res').hide();
+    //task4
+    task4gen();
+    //task5
+    task5.text.forEach(function(el){
+        $('#task5text').append('<p>' + el + '</p>');
+    });
+    $('#task5res').hide();
+    $('#task5task').hide();
+});
+$('#task1start').on('click', function(){
+    $('#task1').hide();
+    $('#task1start').hide();
+    $('#task1test').show();
+    $('#task1footer').show();
+});
+$('#task1submit').on('click', function(){
+    $('#task1test').hide();
+    $('#task1footer').hide();
+    $('#task1results').show();
+    var total = 5;
+    var correct = 0;
+    for(var i = 1; i < 6; i++){
+        if($('input[name="t1q'+ i +'"]:checked').val() == task1.questions[i-1].correct){
+            correct++;
+        }
+    }
+    $('#task1res').text(((correct/total)*100) + '%');
+    $('#task1resDesc').text(task1.results[correct]);
+});
+
+//task2
+$('#task2start').on('click', function(){
+    $('#task2p1').hide();
+    $('#task2footer').show();
+    $('#task2start').hide();
+    $('#task2p2').show();
+
+});
+
+$('#task2submit').on('click', function(){
+    $('#task2p2').hide();
+    $('#task2footer').hide();
+    $('#task2results').show();
+    var t2correct = 0;
+    var t2total = 5;
+    for(var i = 1; i < 6; i++){
+        if($('#task2in'+ i).val().toLowerCase() == task2.answers[i-1]){
+            t2correct++;
+        };
+    };
+    $('#task2res').text(((t2correct/t2total)*100) + '%');
+    $('#task2resDesc').text(task1.results[t2correct]);
+});
+//task3
+var t3total = 20;
+var t3correct = 0;
+var t3cur = 0;
+$('#task3n').on('click', function() {
+    if(t3cur != 3 && t3cur != 9 && t3cur != 16){
+        t3correct++;
+    }
+    t3cur++;
+    $('#task3word').text(task3.words[t3cur]);
+    if(t3cur == 20){
+        task3finish();
+    }
+});
+$('#task3y').on('click', function() {
+    if(t3cur == 3 || t3cur == 9 || t3cur == 16){
+        t3correct++;
+    }
+    t3cur++;
+    $('#task3word').text(task3.words[t3cur]);
+    if(t3cur == 20){
+        task3finish();
+    }
+});
+function task3finish(){
+    $('#task3part').hide();
+    $('#task3res').show();
+    $('#task3result').text(((t3correct/t3total)*100) + '%');
+    $('#task3resDesc').text(task1.results[Math.floor(t3correct/5)]);
+}
+//task4
+var curClick = 1;
+function task4gen(){
+    var numbersarr = [1,2,3,4,5,6,7,8,9,10,
+        11,12,13,14,15,16,17,18,19,20,
+        21,22,23,24,25];
+    numbersarr = shuffle(numbersarr);
+    console.log(numbersarr);
+    var cur = 0;
+    for(var x = 0; x < task4.x; x++){
+        $('#task4table').append('<tr id="x'+ x +'"></tr>')
+        for(var y = 0; y < task4.y; y++){
+            $('#x'+ x +'').append('<td id="'+ numbersarr[cur] +'"><h2>'+ numbersarr[cur] +'</h2></td>');
+            $('#'+ numbersarr[cur]).on('click', createHandler(numbersarr[cur]));
+            cur++;
+        }
+    }
+    $('#task4result').hide();
+}
+
+var start, stop;
+function createHandler(cur){
+    return function(){
+        console.log(cur);
+        console.log(curClick);
+        if(curClick == 1){
+            start = new Date().getTime();
+        }
+        if(curClick == cur){
+            $('#'+cur).css('background-color', 'green');
+            curClick++;
+        }
+        if(curClick == 26){
+            stop = new Date().getTime();
+            var time = stop - start;
+            $('#task4table').hide();
+            $('#task4result').show().text('Your time is: ' + time/1000 + ' seconds!')
+        }
+    }
+}
+//task5
+var t5start, t5stop;
+$('#task5start').on('click', function(){
+    t5start = new Date().getTime();
+    $('#task5start').hide();
+    $('#task5task').show();
+});
+$('#task5stop').on('click', function(){
+    t5stop = new Date().getTime();
+    var t5time = t5stop - t5start;
+    $('#task5res').show().text('Your result is: ' + Math.ceil((1000)/(t5time/1000)) + ' words per second!');
+    $('#task5text').hide();
+    $('#task5stop').hide();
+});
+
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex ;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
---
+++ js/repo.js
@@ -0,0 +1,100 @@
+var task1 = {
+    title: 'This Boy and His Bicycle',
+    author: 'Author: Franklin B. Holleman, Ph.D.',
+    text: ['It’s wonderful to be back in my boyhood hometown again to visit with my now elderly mother. It seems like centuries ago when I was growing up here. Back then, this small town was just a backdrop that formed the unremarkable environment in which I lived my everyday life.',
+    'Mom’s doing great for her age, but she’s moving slower these days. After a lengthy but heartwarming talk in the den about my wife, the kids and how well work is going back in the big city far from here, we have reached the point of being all talked out for now. ',
+    '                                                                                                                                                                                                                                                                     What a perfect time to go for a walk and get some fresh air. The outside loudly calls for a look around the neighborhood to see how things have changed, and how things have stayed the same. As I walk down the street, it’s like I’m on my trusted bicycle riding around as a young boy. I’m on yet another grand mission on my bike again. Oh, the places my bike could take me, and did.',
+    'There’s the small corner store, just a quick bike ride down one street and up another, where I can get an ice-cold soda in a glass bottle with the red metal cap. Inside is the long candy aisle where I must carefully consider my choices; will it be a candy bar, or pack of football cards with the bonus flat piece of bubble gum, or a handful of fireball jawbreakers?  … The freedom to decide continues unabated¸ only the stakes are higher with time, requiring proportionally greater wisdom.',
+    'There’s the bridge over the lazy river where I love to park my bike, and just sit on the tall ledge to think penetrating thoughts, as I look down the river into the expansive horizon. How far does this river go, and what is beyond the river, and even what is beyond that? Maybe someday I can travel to experience it on my own when I get older … Many are the places I’ve since traveled, only to enlarge my curiosity further, as my awareness of the unknown has grown.',
+    'It’s only a quick ride to reach the old retired doctor’s house, whose lawn I mow weekly. He pays me a modest wage to help maintain his large yard. We always enjoy talking as we work together. I don’t remember exactly all that we talked about, but I do remember that he always listened and I felt appreciated. He sure did know a lot about what life has in store that is common to every man … His example of deliberate kindness and thoughtful wisdom showed me how to pass on the same to others younger than myself, many times over the years.',
+    'Up ahead is her house, just three houses down from that corner. I sure do have a crush on her. She is so lovely with pretty blond hair. I need to ride past her house yet again to see if I might find her outside where I could maybe, just maybe, have the chance to talk with her after school. I’m not sure what to say given the chance, but it’s worth the risk. Too bad she never really noticed me at school, even though we shared the same classes; I wonder where she is now … Years later in college, it was just as awkward when I first met my future bride and struggled to start a conversation, but those moments surprisingly became the initial spark that turned into the real communication of our now 25-year marriage.',
+    'Where is that special tree? I know it’s somewhere around here between these two houses, or is it the next house? I guess the tree is now long gone. Having packed a peanut butter and jelly sandwich lunch in my bicycle basket, I’m set to spend almost the entire day climbing around in it, where I can think and dream unrestrained. It is here I have found a glorious refuge; each large branch is its own lavish room comprising my expansive estate. What a rich man I am to have found such an interesting place that I can call my own!  I love to climb to the very top where the highest branch is so narrow, I must hold on tight as I sway widely with it in the wind – because the higher I go, the more I can see. I can even see past the supermarket, with a glimpse of the ocean a mile beyond that. I’m glad my mother never knew how high I dared to climb … Ever since then, I have always found it well worth paying the price to reach the vantage point that affords life’s best perspective.',
+    'My bicycle enabled so much adventure along the safe sidewalks and sleepy streets of my town. It offered great freedom to explore. There were so many places to ride my bike, including special places that only I knew about. There were seemingly endless opportunities for discovery, compelling experiences, and even the thrill of imagined danger.',
+    'My life then had space wide enough to ride but secure enough with all its well-defined boundaries, where I purposed to set my course towards creating an engaging life to enjoy. This was a place that couldn’t have been more adventurous.',
+    'It’s been decades since those days of boyhood exploration, and I’m sure that rust has long since consumed my old bike.  But even though everything is different now, nothing has changed. Life remains an adventure that continually beckons.  Although I am more established now, I still fashion the places I call my own so that I may live large. I still must plan to set a direction and be careful to navigate a wise path.',
+    'This wonderful little town - this special appointed place - provided a significant time of preparation which was a microcosm of my life, where I learned, without realizing I was learning, the most important things before I ever lived them.'
+    ],
+    questions: [
+        {
+            text: '1.	Is lyrical hero men or women?',
+            options: ['men', 'women', 'androgyny'],
+            correct: 1,
+        },
+        {
+            text: '2.	What did lyrical hero found on the street of the town?',
+            options: ['old football ball', 'bike', 'boomerang'],
+            correct: 2,
+
+        },
+        {
+            text: '3.	Where lyrical hero love to park his bike?',
+            options: ['Near local shop', 'In the garage', 'On the bridge'],
+            correct: 3,
+        },
+        {
+            text: '4.	Which place lyrical hero can call as his refuge?',
+            options: ['Park near bridge', 'His garage', 'Old tree'],
+            correct: 3,
+        },
+        {
+            text: '5.	Lyrical hero imagine his life like:',
+            options: ['adventure', 'fight', 'void'],
+            correct: 1,
+        },
+    ],
+    results: [  'Poor! You must work hard to master your reading skillz.',
+                'Better than nothing! Practise more and you will see the benefits.',
+                'Average result. Keep practising and you can become better reader.',
+                'Good job! Your speed reading skills are better than average, but there is still opportunity for improvement',
+                'Excellent! You are best of the best, but don\'t forget to practise'
+    ],
+};
+var task2 = {
+    text1: 'Although he doesn\'t know it yet, on January 14th, at 73 years of age, Andrew will rest eternally at his late wife\'s side. On March 4th, at 29 years, he and his wife will have their first child. On October 18th, when he is 26, they will be married in a small, quaint ceremony among family, and in just a few minutes, at the age of 24, he will meet his wife-to-be at this coffee shop after accidentally spilling coffee on her. I know these things because I am here to make sure he trips.',
+    text2: 'Although he doesn\'t know it yet, on PLACEHOLDER 14th, at 73 years of age, PLACEHOLDER will rest eternally at his late wife\'s side. On March 4th, at 29 years, he and his wife will have their first child. On October 18th, when he is 26, they will be PLACEHOLDER. in a small, quaint ceremony among family, and in just a few minutes, at the age of PLACEHOLDER., he will meet his wife-to-be at this PLACEHOLDER. shop after accidentally spilling coffee on her. I know these things because I am here to make sure he trips.'.split('PLACEHOLDER'),
+    answers: [  'january',
+                'andrew',
+                'married',
+                '24',
+                'coffee']
+};
+
+var task3 = {
+  words: [
+      'pedal',
+      'whimsical',
+      'spot',
+      'whimsical', //3
+      'immense',
+      'lamentable',
+      'pat',
+      'dependent',
+      'dusty',
+      'dependent', //9
+      'nappy',
+      'nose',
+      'quarter',
+      'wasteful',
+      'wiener',
+      'bore',
+      'nappy', //16
+      'legs',
+      'graceful',
+      'stingy'
+  ],
+};
+var task4 = {
+    x: 5,
+    y: 5,
+}
+
+var task5 = {
+    text: [
+        'It was Thanksgiving again, which meant that my family was making the long trek eastward to be around my siblings and their families back at the Old Home Place. We had gathered each of the past years since our father had passed away, but this was the first year that my mother, the last of that generation, was no longer around. My brothers and sister and I all lead very different lives, and were scattered about the country with our own families and jobs to keep us apart; this was one of the only times when we were all a big happy family again.',
+        'My family\'s flight was delayed, so as we pulled up in our rental car, we were the last group to arrive. The preparations were in full swing, and I could smell the turkey wafting from the kitchen, as I opened my door. We had hardly gotten out when we were wrapped in many warm embraces welcoming us home. We all exchanged our greetings, and my wife went to join the other women as they busied themselves with preparing the meal and catching up; my sister was pregnant again, so they had plenty to talk about. I retired to the back porch with my two brothers and brother-in-law, and sat down in a rocking chair with a sigh. My two youngest went off to find their cousins, but my eldest son followed me and sat cross-legged on the floor, craning forward, quietly and intently listening to the conversation. This was the first year that he was eager to grow up and find a place among the men. I thought I might tell him that adulthood was not so glamorous as he supposed, but I knew he would only understand that once it was too late.',
+        'As the sun began to sit lower in the sky, my youngest brother\'s wife called everyone in to dinner. It took little prodding to get the kids to all come in once they understood it was time to eat. The excited murmur grew louder as the whole group collected in the old dining room. We formed a circle around the long, wooden table as we held hands and I, the oldest, and now the head of the family, blessed the meal. When I finished, my wife instructed that we were serving oldest to youngest and that the kids would \'just have to wait\'. Eventually, everyone had made it through the line and had filled their plates with the incredible spread of food.',
+        'As we gathered around the old table, the memories flowed as freely as the iced tea. We shared amusing anecdotes, revived old memories of our parents and childhood, and all around celebrated the years long passed. At a pause in the conversation, I looked down for a moment, over to the other table to check on the little ones. All my children and nieces and nephews were enjoying themselves - all except for my oldest son. While the others were enamored in child\'s banter, he gave me just the kind of look I remembered giving my dad when I was his age; his eyes sighed his protest of being relegated to the kid\'s table. He silently pleaded with me, almost as if to ask me why I was so intent on keeping him in the ignorance of childhood.',
+        'After the meal, everyone minded their overstuffed stomachs and reclined on the back porch of the old house. Overcome with nostalgia, I decided to break from the reclining group, and took a walk down to the cemetery where my aunts, uncles, mother, and father lay. I started off down the path that continued about a quarter mile through the woods. The autumn trees bowed over the walkway forming a canopy, while the cool, crisp air, which, carried by a soft breeze comforted me, and fostered a sense of timelessness as I walked alone thinking. Brown, brittle leaves fell carelessly in front of my path while my mind was off remembering. We used to walk this path together, as a family. For us, Thanksgiving is not only a time of celebration of family and blessings, but also a time to remember relatives long deceased and to enjoy the memories. My siblings and I would always cut up and lag behind the group, which would inspire disapproving looks from my mother. She felt that this was a very somber event – something I had no grasp of as a child.',
+        'I slowly approached the gate and I paused for a moment. It’s so still here, so quiet, so peaceful. I walked past rows of gravestones as I finally approached the site where my relatives lay. To many, visiting their relatives in a cemetery can be a sad experience; I however, was happy for them, because they still gathered together as they had before. I smiled as I remembered those times, when I was but a child and I could just barely see over the table. I could hear their talking and laughter again. How warm and familiar it all was! I remembered how laughter would erupt after my father told a good joke – he was always telling jokes. I could even smell the feast my mother and aunts would cook for us, and could almost feel them slapping my hand as I tried to sneak a piece of turkey before the meal. I remembered sitting at the smaller table with my cousins and siblings, feeling as if I were too old to sit at the kid\'s table. I remembered feeling left out, as if the adults kept some kind of grown-up secret from me. That same feeling I felt again, as I stood there seeing them all lying as they used to sit, in those two long rows. Although I had grown to be an adult, they still seemed to keep some secret from me, one that I was not to know of yet, one that I am not ready yet to learn.'
+    ]
+};
